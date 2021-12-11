60d6c4c2b39e72fc1980af9116c1069704b3b143
Nguyen Thanh Son <nguyenthanhson.sowntee@gmail.com>
2021-12-11 18:59:12 +0700

    Add files via upload

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,7 @@
+Import-Module posh-git
+Import-Module oh-my-posh
+Import-Module PSReadLine
+Import-Module -Name Terminal-Icons
+Set-PoshPrompt -Theme custom
+Set-PSReadLineOption -PredictionSource History
+clear
